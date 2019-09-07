4bd2b595702243f8da4322c38fb7d90b9040ade1
Pierre Schmitz <pierre@archlinux.de>
2019-09-07 13:11:56 +0200

    Simplify code

--- index.php
+++ index.php
@@ -1,94 +1,88 @@
 <?php
 
-$request = 'index.php';
+$path = 'index.php';
 
 if (isset($_GET['page'])) {
 	switch ($_GET['page']) {
 		case 'ForgotPassword':
-			$request = 'login.php?action=forget';
+			$path = 'login.php?action=forget';
 			break;
 		case 'Forums':
-			$request = 'index.php';
+			$path = 'index.php';
 			break;
 		case 'GetRecent':
-			$request = 'extern.php?action=feed&type=atom&show=25&order=posted';
+			$path = 'extern.php?action=feed&type=atom&show=25&order=posted';
 			if (isset($_GET['forum'])) {
-				$request .= '&fid='.intval($_GET['forum']);
+				$path .= '&fid=' . intval($_GET['forum']);
 			}
 			break;
 		case 'Login':
-			$request = 'login.php';
+			$path = 'login.php';
 			break;
 		case 'MarkupHelp':
-			$request = 'help.php';
+			$path = 'help.php';
 			break;
 		case 'NewPost':
 			if (isset($_GET['thread'])) {
-				$request = 'post.php?tid='.intval($_GET['thread']);
+				$path = 'post.php?tid=' . intval($_GET['thread']);
 			}
 			break;
 		case 'NewThread':
 			if (isset($_GET['forum'])) {
-				$request = 'post.php?fid='.intval($_GET['forum']);
+				$path = 'post.php?fid=' . intval($_GET['forum']);
 			}
 			break;
 		case 'Postings':
 			if (isset($_GET['thread'])) {
-				$request = 'viewtopic.php?id='.intval($_GET['thread']);
+				$path = 'viewtopic.php?id=' . intval($_GET['thread']);
 				if (isset($_GET['post'])) {
-					$request .= '&p='.(floor(intval($_GET['post']) / 25) + 1);
+					$path .= '&p=' . (floor(intval($_GET['post']) / 25) + 1);
 				}
 			}
 			break;
 		case 'QuotePost':
 			if (isset($_GET['post'])) {
-				$request = 'viewtopic.php?pid='.intval($_GET['post']).'#p'.intval($_GET['post']);
+				$path = 'viewtopic.php?pid=' . intval($_GET['post']) . '#p' . intval($_GET['post']);
 			}
 			break;
 		case 'Recent':
-			$request = 'search.php?action=show_24h';
+			$path = 'search.php?action=show_24h';
 			break;
 		case 'Register':
-			$request = 'register.php';
+			$path = 'register.php';
 			break;
 		case 'Search':
-			$request = 'search.php';
+			$path = 'search.php';
 			if (isset($_GET['search'])) {
-				$request .= '?action=search&show_as=topics&keywords='.urlencode($_GET['search']);
+				$path .= '?action=search&show_as=topics&keywords=' . urlencode($_GET['search']);
 			}
 			break;
 		case 'SearchResults':
-			$request = 'search.php';
+			$path = 'search.php';
 			break;
 		case 'ShowUser':
 			if (isset($_GET['user'])) {
-				$request = 'profile.php?id='.intval($_GET['user']);
+				$path = 'profile.php?id=' . intval($_GET['user']);
 			}
 			break;
 		case 'Threads':
 			if (isset($_GET['forum'])) {
-				$request = 'viewforum.php?id='.intval($_GET['forum']);
+				$path = 'viewforum.php?id=' . intval($_GET['forum']);
 				if (isset($_GET['thread'])) {
-					$request .= '&p='.(floor(intval($_GET['thread']) / 30) + 1);
+					$path .= '&p=' . (floor(intval($_GET['thread']) / 30) + 1);
 				}
 			}
 			break;
 		case 'UserRecent':
 			if (isset($_GET['user'])) {
-				$request = 'search.php?action=show_user&user_id='.intval($_GET['user']);
+				$path = 'search.php?action=show_user&user_id=' . intval($_GET['user']);
 			}
 			break;
 		default:
-			header('HTTP/1.1 404 Not Found');
-			echo '<html><head><title>Not Found</title></head><body><p>Page <strong>'.htmlspecialchars($_GET['page']).'</strong> was not found</p></body></html>';
+			http_response_code(404);
 			exit();
 	}
 }
 
-$redirect = 'https://bbs.archlinux.de/'.$request;
-
-header('HTTP/1.1 301 Moved Permanently');
-header('Location: '.$redirect);
-
-echo '<html><head><title>Moved Permanently</title></head><body><p>Moved permanently to <a href="'.htmlspecialchars($redirect).'">'.htmlspecialchars($redirect).'</a></p></body></html>';
-exit();
+http_response_code(301);
+header(sprintf('Location: https://bbs.archlinux.de/%s', $path));
