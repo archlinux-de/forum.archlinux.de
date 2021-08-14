4006ce461eba8affc1c46926699632053b173f6c
Pierre Schmitz <pierre@archlinux.de>
2021-08-14 15:02:23 +0200

    Test basic FluxBB and LL redirects

---
+++ tests/e2e/cypress/integration/RedirectFluxBB.spec.js
@@ -0,0 +1,24 @@
+describe('RedirectFluxBB', () => {
+  it('redirects viewtopic.php', () => {
+    const userName = 'tester' + Math.random().toString().replace('.', '')
+    const postTitle = 'Hello World!'
+    const postMessage = 'What`s up?'
+
+    cy.register(userName, 'password1234', userName + '@archlinux.de')
+
+    cy.get('button.IndexPage-newDiscussion').click()
+    cy.get('.item-discussionTitle input').type(postTitle)
+    cy.get('textarea.TextEditor-editor').type(postMessage)
+    cy.get('a.DiscussionComposer-changeTags').click()
+    cy.get('.SelectTagListItem-name').click()
+    cy.get('button[type=submit]').click()
+    cy.get('.item-submit button').click()
+    cy.url().should('contain', Cypress.config().baseUrl + '/d/')
+
+    cy.url().then(urlString =>{
+      const discussionId = urlString.replace(/.*\/d\/([0-9]+)-.*/, '$1')
+      cy.visit(`${Cypress.config().baseUrl}/viewtopic.php?id=${discussionId}`)
+      cy.url().should('contain', `${Cypress.config().baseUrl}/d/${discussionId}-`)
+    })
+  })
+})
