425d0322f25693f95175a3a15ecdb9ff65ceac71
Pierre Schmitz <pierre@archlinux.de>
2026-03-04 08:58:15 +0100

    Revert "Add app:backup-images command to download external images"
    This reverts commit bab9ed03b8f3d207c4962742e049045f8ee4a495.

--- extend.php
+++ extend.php
@@ -7,7 +7,6 @@
  * LICENSE file that was distributed with this source code.
  */
 
-use App\Console\BackupImages;
 use App\Console\EnableExtensions;
 use App\Middleware\ContentSecurityPolicy;
 use App\ServiceProvider\ErrorLogProvider;
@@ -18,7 +17,6 @@ use s9e\TextFormatter\Configurator\Items\Tag;
 
 return [
     (new Extend\Console())->command(EnableExtensions::class),
-    (new Extend\Console())->command(BackupImages::class),
     (new Extend\ServiceProvider())->register(ErrorLogProvider::class),
     (new Extend\ServiceProvider())->register(SessionServiceProvider::class),
     (new Extend\Middleware('forum'))->add(ContentSecurityPolicy::class),
