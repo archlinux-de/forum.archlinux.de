246e672a837805b1632ec78cbec7fd6285625da4
Pierre Schmitz <pierre@archlinux.de>
2026-03-04 08:56:56 +0100

    Revert "Add app:validate-posts command for content and metadata checks"
    This reverts commit 7bafead709430cee318a0525a4aa2af265fcf83b.

--- extend.php
+++ extend.php
@@ -10,7 +10,6 @@
 use App\Console\BackupImages;
 use App\Console\EnableExtensions;
 use App\Console\ImportImages;
-use App\Console\ValidatePosts;
 use App\Middleware\ContentSecurityPolicy;
 use App\ServiceProvider\ErrorLogProvider;
 use App\ServiceProvider\SessionServiceProvider;
@@ -22,7 +21,6 @@ return [
     (new Extend\Console())->command(EnableExtensions::class),
     (new Extend\Console())->command(BackupImages::class),
     (new Extend\Console())->command(ImportImages::class),
-    (new Extend\Console())->command(ValidatePosts::class),
     (new Extend\ServiceProvider())->register(ErrorLogProvider::class),
     (new Extend\ServiceProvider())->register(SessionServiceProvider::class),
     (new Extend\Middleware('forum'))->add(ContentSecurityPolicy::class),
